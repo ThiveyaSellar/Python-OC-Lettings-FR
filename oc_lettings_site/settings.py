8ed2f5d176bd34d117df58517c7d734dfc965f80
ThiveyaSellar <sellarthiveya@gmail.com>
2025-11-20 20:56:55 +0100

    environment variable for sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -126,7 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 sentry_sdk.init(
-    dsn=config.get('sentry', 'dsn'),
+    # dsn=config.get('sentry', 'dsn'),
+    SENTRY_DSN = os.environ.get("SENTRY_DSN", ""),
     # Add data like request headers and IP for users;
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     integrations=[DjangoIntegration()],
