804f4ed350a06d6a45a6e909724cc18a09efde19
ThiveyaSellar <sellarthiveya@gmail.com>
2025-11-24 12:55:52 +0100

    Changing settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['oc-lettings-site-h6zc.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
