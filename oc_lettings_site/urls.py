f87c6ff65bee0787d4149480dad2a5726efade41
ThiveyaSellar <sellarthiveya@gmail.com>
2025-11-25 17:01:04 +0100

    Settings change

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import views
+import sentry_dsk
+
+
+def test_error(request):
+    try:
+        division_by_zero = 1 / 0
+        print(division_by_zero)
+    except ZeroDivisionError as e:
+        sentry_sdk.capture_exception(e)  # Envoie l'exception à Sentry manuellement
+        return HttpResponse("Erreur capturée et envoyée à Sentry.")
 
 
 urlpatterns = [
