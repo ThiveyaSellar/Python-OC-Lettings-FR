b682f73372728c8283c1774e1cc4138b8bb4580d
ThiveyaSellar <sellarthiveya@gmail.com>
2025-11-25 16:47:46 +0100

    Settings change

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -7,12 +7,15 @@ from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
 
+
 def test_error(request):
     try:
         division_by_zero = 1 / 0
+        print(division_by_zero)
     except ZeroDivisionError:
-        logger.exception("Une erreur de division par zéro a été capturée et envoyée à Sentry.")
-        return HttpResponse("Erreur capturée et envoyée à Sentry via le logger.")
+        logger.exception("Une erreur  Sentry.")
+        return HttpResponse("Erreur capturée et envoyée à .")
+
 
 urlpatterns = [
     path('', views.index, name='index'),
