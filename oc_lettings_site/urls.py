90ab5fa8f1e0ec6c64f4f2578b1943ac674ff1aa
ThiveyaSellar <sellarthiveya@gmail.com>
2025-11-25 16:58:13 +0100

    Settings change

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,19 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import views
-import logging
-from django.http import HttpResponse
-
-logger = logging.getLogger(__name__)
-
-
-def test_error(request):
-    try:
-        division_by_zero = 1 / 0
-        print(division_by_zero)
-    except ZeroDivisionError:
-        logger.error("Une erreur  Sentry.")
-        return HttpResponse("Erreur capturée et envoyée à .")
 
 
 urlpatterns = [
@@ -22,5 +9,4 @@ urlpatterns = [
     path('lettings/', include('lettings.urls', namespace="lettings")),
     path('profiles/', include('profiles.urls', namespace="profiles")),
     path('admin/', admin.site.urls),
-    path('test-error/', test_error),
 ]
