d3e1a30368ee72e0593860f057c747c0199f5855
ThiveyaSellar <sellarthiveya@gmail.com>
2025-12-04 17:00:45 +0100

    Test error 500

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,9 +4,13 @@ from django.urls import path, include
 from . import views
 
 
+def trigger_500(request):
+    return 1/0
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace="lettings")),
     path('profiles/', include('profiles.urls', namespace="profiles")),
     path('admin/', admin.site.urls),
+    path('test-500/', trigger_500),
 ]
