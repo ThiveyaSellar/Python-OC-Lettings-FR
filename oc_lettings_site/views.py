ad2075194d6208a650a4661027db8bbe89c3f221
ThiveyaSellar <sellarthiveya@gmail.com>
2025-12-05 16:13:17 +0100

    Adding exception to trigger_500_error

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -22,9 +22,14 @@ def index(request):
         django.http.HttpResponse:
         The rendered HTML page for the homepage ('index.html').
     """
-    logger.error("Page d’accueil consultée")
+    logger = logging.getLogger(__name__)
+    logger.info("Page d’accueil consultéee")
     return render(request, 'index.html')
 
 
 def trigger_500_error(request):
-    return 1/0
+    try:
+        1 / 0
+    except Exception as e:
+        logger.error("Erreur serveur, division par zero")
+        return render(request, "500.html", status=500)
